81361c026eed6e9f2061f61c4764712a685aab93
Mike Roberts <mike.roberts.2k10@googlemail.com>
2020-01-23 13:47:21 +0000

    2301

--- my_commands/utils/actions.py
+++ my_commands/utils/actions.py
@@ -15,20 +15,15 @@ class SlowKey(KeyBase):
     interval_default = 1.0
 
 class Read(ActionBase):
-    def __init__(self, name=None, remove_cr=False, same_is_okay=True):
+    def __init__(self, name=None):
         ActionBase.__init__(self)
         self.name = name
-        self.remove_cr = remove_cr
-        self.same_is_okay = same_is_okay
 
-    def read_selected(self, same_is_okay=False, wait=5):
+    def read_selected(self, wait=5):
         key_wait = 0.01 * wait
         cb = Clipboard(from_system=True)
         time.sleep(key_wait)
-        temporary = None
-        # prior_content = None
         try:
-            # prior_content = Clipboard.get_system_text()
             Clipboard.set_system_text("")
             Key("c-c").execute()
             time.sleep(key_wait)
@@ -39,7 +34,7 @@ class Read(ActionBase):
         return temporary
 
     def _execute(self, data=None):
-        text = self.read_selected(self.same_is_okay)
+        text = self.read_selected()
         if data:
             data[self.name] = text
         return True
